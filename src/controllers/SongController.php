d36e4e226210d12275c597628c289b00ebcdc880
TryaNever <alexispernette@outlook.fr>
2025-04-09 14:27:33 +0200

    patch call class

--- src/controllers/SongController.php
+++ src/controllers/SongController.php
@@ -7,12 +7,12 @@ class SongController extends Controller
 {
     private $songServiceBDD;
     public function __construct() {
-        $this->songServiceBDD = EntityManager::class;
+        $this->songServiceBDD = new EntityManager();
     }
     public function index() {
         $data = $this->songServiceBDD->executeQuery('SELECT * FROM `chanson` ch
 INNER JOIN categorie ca ON ch.id_categorie = ca.id_categorie
-INNER JOIN chanteur cha ON cha.id_chanteur = ch.id_chanteur');
+INNER JOIN chanteur cha ON cha.id_chanteur = ch.id_chanteur', []);
         $this->render('allSong.html.twig', ['songs' => $data]);
     }
 }
